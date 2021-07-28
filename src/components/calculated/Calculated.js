3f6498b82b83160f2883f82b6024ee27a0c7f58d
Virus936 <lok.vira@gmail.com>
2021-07-28 12:25:07 +0200

    calculated display fixed

--- src/components/calculated/Calculated.js
+++ src/components/calculated/Calculated.js
@@ -4,29 +4,44 @@ function Calculated({bill, tip, person, reset}){
   return(
     <Container>
 
-      <p>
-        Tip Amount
-      </p>
-      <p>
-        {Math.round((bill*tip/person)*100)/100} / person
-      </p>
+      <div className='Calculated-Element'>
+        <div>
+          <p>
+          Tip Amount
+          </p>
+          <p>
+          / person
+          </p>
+        </div>
+        <Result>
+        {Math.round((bill*tip/person)*100)/100}
+        </Result>
+      </div>
 
-
-      <p>
-        Total
-      </p>
-      <p>
-        {Math.round((bill*(1+tip)/person)*100)/100} / person
-      </p>
-
+      <div className='Calculated-Element'>
+        <div>
+          <p>
+          Total
+          </p>
+          <p>
+           / person
+          </p>
+        </div>
+        <Result>
+        {Math.round((bill*(1+tip)/person)*100)/100}
+        </Result>
+      </div>
       <Button onClick={reset}>Reset</Button>
     </Container>
     )
 }
 
 
+const Result = styled.div`
+  color: hsl(172, 67%, 45%);
+  font-size:3em;
+`
 const Button = styled.button`
-
   flex: 1 1 120px;
   background-color: hsl(172, 67%, 45%);
   color:white;
@@ -47,5 +62,9 @@ const Container = styled.div`
   border-radius:1em;
   color:white;
   padding:1em;
+  .Calculated-Element{
+    display:flex;
+    justify-content:space-between;
+  }
 `
 export default Calculated
